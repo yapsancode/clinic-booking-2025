30fe5d3a266cdd094ca02232d8d29905c035bae0
Isyraf <isyrafsan97@gmail.com>
2025-10-21 01:36:39 +0800

    Hiding the booking page

--- src/app/(main)/book-appointment/page.tsx
+++ src/app/(main)/book-appointment/page.tsx
@@ -1,7 +1,17 @@
 import { BookingProvider } from "@/context/BookingContext";
 import BookingForm from "./BookingForm";
+import { notFound } from "next/navigation";
 
 export default function BookAppointmentPage() {
+  
+  // temporary logic to hide this page
+  const isInvalid = true
+
+  if (isInvalid) {
+    notFound();
+  }
+
+  
   return (
     <BookingProvider>
       <BookingForm />
