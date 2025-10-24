cfbb34d2dcf61b23b5cac6f450c3afd49951b9a7
Isyraf <isyrafsan97@gmail.com>
2025-10-25 00:09:05 +0800

    update api

--- src/lib/api/blogs.ts
+++ src/lib/api/blogs.ts
@@ -3,18 +3,16 @@ import { Blogs } from "@/types/blogs";
 const API_BASE_URLV2 = "https://clinic-system-2025-backend.fly.dev/api";
 
 export async function fetchPromotions(): Promise<Blogs[]> {
-  // console.log("Fetching from:", `${API_BASE_URL}/promotions`);
-  
   const res = await fetch(`${API_BASE_URLV2}/blogs`, {
-    cache: 'no-store', // or 'force-cache' depending on your needs
+    cache: "no-store",
   });
-  
+
   if (!res.ok) {
     throw new Error(`Failed to fetch promotions: ${res.status}`);
   }
-  
+
   const data = await res.json();
   console.log("Fetched promotions:", data);
-  
+
   return data; // API returns array directly
 }
--- src/types/blogs.ts
+++ src/types/blogs.ts
@@ -1,11 +1,10 @@
 export interface Blogs {
   id: number;
-  image: string;
   title: string;
-  description: string;
-  discount: string;
-  validUntil: string;
-  buttonText: string;
-  buttonLink: string;
-  badge: string;
+  content: string;
+  author: string;
+  thumbnail: string;
+  tags: string; // could be string[] if your API ever returns an array instead
+  dateAdded: string | null;
+  lastUpdated: string | null;
 }
